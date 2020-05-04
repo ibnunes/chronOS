80d7bbfe40b8695958e7c95f3af4a763adc3f69f
Jorge <jocapissarra@protonmail.com>
2020-05-04 19:26:23 +0100

    created data structures

--- manager.h
+++ manager.h
@@ -1,1 +1,13 @@
 #include"simulator.h"
+
+typedef struct{
+    char ins;
+    int n;
+    char nome[15]
+}instruction;
+
+typedef struct {
+    char nome;
+    int start;
+    // add whatever else is needed
+}PCB
--- processor.h
+++ processor.h
@@ -1,6 +1,11 @@
 #include"simulator.h"
 
 typedef struct{
-    int id;
-    // add other stuff
+    int id;         // identificador do processo
+    int state;      //estado do processo
+    int counter;    // contador do programa
+    int pid;        //identificador do processo progenitor
+    int priority;   //prioridade do processo
+    int time_limit; //prazo temporal do processo
+    // add whatever else is needed
 } process;
