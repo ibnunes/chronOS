6fbcf1e86ac2839433fa6f59e231461d6987789d
Igor Nunes <igor.b.nunes@gmail.com>
2020-05-09 19:09:30 +0100

    Added initial comments to all header files

--- include/processor.h
+++ include/processor.h
@@ -1,3 +1,21 @@
+//------------------------------------------------------------------------------
+//
+// chronOS - A scheduling simulator
+//
+//    Copyright (C) 2020 Igor Cordeiro Bordalo Nunes (www.igornunes.com)
+//    Copyright (C) 2020 Jorge Miguel Louro Pissarra
+//    Copyright (C) 2020 Diogo Castanheira Simões
+//    Copyright (C) 2020 Universidade da Beira Interior (www.ubi.pt)
+//
+// RUNTIME LIBRARIES PACKAGE
+//    processor.h
+//
+// DESCRIPTION:
+// -----------
+// Process management.
+//------------------------------------------------------------------------------
+
+
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
@@ -22,14 +40,28 @@ typedef struct {
  * 'T' -> Terminated
  */
 
+/* Processes an instruction from a process */
 void processorFunction(instruction*, process*);
 
+/* M n: mudar o valor da variável para o valor n. */
 void changeValue(process*, int);
+
+/* A n: adicionar o valor n à variável. */
 void addValue(process*, int);
+
+/* S n: subtrair o valor n à variável. */
 void subtractValue(process*, int);
+
+/* B: bloquear este processo. */
 void blockProcess(process*);
+
+/* T: terminar este processo. */
 void terminateProcess(process*);
+
+/* C n: criar um novo processo filho, o processo pai executa n linhas à frente. */
 process *createNewProcess(process*, int);
+
+/* L: limpar o programa atual e substituir pelo novo. */
 void cleanProgram(char*);
 
 #endif
--- include/simulator.h
+++ include/simulator.h
@@ -1,3 +1,22 @@
+//------------------------------------------------------------------------------
+//
+// chronOS - A scheduling simulator
+//
+//    Copyright (C) 2020 Igor Cordeiro Bordalo Nunes (www.igornunes.com)
+//    Copyright (C) 2020 Jorge Miguel Louro Pissarra
+//    Copyright (C) 2020 Diogo Castanheira Simões
+//    Copyright (C) 2020 Universidade da Beira Interior (www.ubi.pt)
+//
+// RUNTIME LIBRARIES PACKAGE
+//    simulator.h
+//
+// DESCRIPTION:
+// -----------
+// Simulation management.
+// The main header file that aggregates everything.
+//------------------------------------------------------------------------------
+
+
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
 
