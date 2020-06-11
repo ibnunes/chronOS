6bf6b2f2f49ef6dbcf93fe7a8558a38858762f54
Igor Nunes <igor.b.nunes@gmail.com>
2020-06-11 22:28:49 +0100

    Adds next-fit, updates heap memory manager and clocks allocation times

--- include/data.h
+++ include/data.h
@@ -23,7 +23,10 @@
 #include <time.h>
 
 MEMORY  *memory;         // Memória
-HEAP    *heap;           // Memória heap
+HEAP    *heap_first;     // Memória heap - componente para first-fit
+HEAP    *heap_next;      // Memória heap - componente para next-fit
+HEAP    *heap_best;      // Memória heap - componente para best-fit
+HEAP    *heap_worst;     // Memória heap - componente para worst-fit
 PCB     *pcb;            // Tabela PCB (Process Control Block)
 clock_t cputime;         // Tempo atual do processador em milissegundos
 plan_q  *plan;           // Plano: lista de processos e tempos de entrada
--- include/heapmgr.h
+++ include/heapmgr.h
@@ -9,14 +9,16 @@
 #define HEAP_FREE_FAILURE -1    // Erro: a memória heap não pôde ser libertada
 
 #define HEAP_ALG_FIRST 1        // Algoritmo de alocação: First-fit
-#define HEAP_ALG_BEST  2        // Algoritmo de alocação: Best-fit
-#define HEAP_ALG_WORST 3        // Algoritmo de alocação: Worst-fit
+#define HEAP_ALG_NEXT  2        // Algoritmo de alocação: Next-fit
+#define HEAP_ALG_BEST  4        // Algoritmo de alocação: Best-fit
+#define HEAP_ALG_WORST 8        // Algoritmo de alocação: Worst-fit
 
 int heapalloc(const int pid, const int size, const int __algorithm);
 int heapfree(const int pid);
 int heapfragcount(void);
 
 int heapalloc_first(const int pid, const int size);
+int heapalloc_next(const int pid, const int size);
 int heapalloc_best(const int pid, const int size);
 int heapalloc_worst(const int pid, const int size);
 
